8b30b88bbde88340747c1bcde13b632ca5dbb049
Christoph Bonitz <ml.christophbonitz@gmail.com>
2015-12-13 21:12:20 +0100

    refactoring of jira query

--- main.go
+++ main.go
@@ -14,6 +14,29 @@ func checkError(err error) {
 	}
 }
 
+func runJqlQuery(config map[string]interface{}, jql string) int {
+	username := config["jiraUsername"].(string)
+	password := config["jiraPassword"].(string)
+	host := config["jiraHost"].(string)
+
+	client := &http.Client{}
+	params := url.Values{}
+	params.Add("jql", jql)
+	params.Add("fields", "key")
+	req, err := http.NewRequest("GET", host+"/rest/api/latest/search?"+params.Encode(), nil)
+	req.SetBasicAuth(username, password)
+	resp, err := client.Do(req)
+	checkError(err)
+	defer resp.Body.Close()
+	rawBody, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+	var jsonResult interface{}
+	err = json.Unmarshal(rawBody, &jsonResult)
+	checkError(err)
+	m := jsonResult.(map[string]interface{})
+	return int(m["total"].(float64))
+}
+
 func main() {
 	rawConfig, configErr := ioutil.ReadFile("./config.json")
 	checkError(configErr)
@@ -21,28 +44,13 @@ func main() {
 	configErr = json.Unmarshal(rawConfig, &jsonConfig)
 	checkError(configErr)
 	config := jsonConfig.(map[string]interface{})
-	host := config["host"].(string)
-	username := config["username"].(string)
-	password := config["password"].(string)
 	queries := config["queries"].([]interface{})
 	for _, queryObject := range queries {
-		client := &http.Client{}
-		params := url.Values{}
 		q := queryObject.(map[string]interface{})
-		params.Add("jql", q["jql"].(string))
-		params.Add("fields", "key")
-		req, err := http.NewRequest("GET", host+"/rest/api/latest/search?"+params.Encode(), nil)
-		req.SetBasicAuth(username, password)
-		resp, err := client.Do(req)
-		checkError(err)
-		defer resp.Body.Close()
-		rawBody, err := ioutil.ReadAll(resp.Body)
-		checkError(err)
-		var jsonResult interface{}
-		err = json.Unmarshal(rawBody, &jsonResult)
-		checkError(err)
-		m := jsonResult.(map[string]interface{})
-		fmt.Printf("%v: %d issues found\n", q["name"], int(m["total"].(float64)))
+		jql := q["jql"].(string)
+		measurement := q["name"]
+		count := runJqlQuery(config, jql)
+		fmt.Printf("%v: %d issues found\n", measurement, count)
 	}
 
 }
