5da9b027c62cafc24ba16786371b9ec73c569aca
Christoph Bonitz <ml.christophbonitz@gmail.com>
2015-12-13 22:02:36 +0100

    now writing to influx

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/influxdb/influxdb/client/v2"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 func checkError(err error) {
@@ -15,9 +17,9 @@ func checkError(err error) {
 }
 
 func runJqlQuery(config map[string]interface{}, jql string) int {
+	host := config["jiraHost"].(string)
 	username := config["jiraUsername"].(string)
 	password := config["jiraPassword"].(string)
-	host := config["jiraHost"].(string)
 
 	client := &http.Client{}
 	params := url.Values{}
@@ -37,7 +39,46 @@ func runJqlQuery(config map[string]interface{}, jql string) int {
 	return int(m["total"].(float64))
 }
 
+func createInfluxClient(config map[string]interface{}) client.Client {
+	host := config["influxHost"].(string)
+	username := ""
+	password := ""
+	if config["influxPassword"] != nil {
+		username = config["influxUsername"].(string)
+		password = config["influxPassword"].(string)
+	}
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     host,
+		Username: username,
+		Password: password,
+	})
+	checkError(err)
+	return c
+}
+
+func createBatchPoints(config map[string]interface{}, c client.Client) client.BatchPoints {
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  config["influxDB"].(string),
+		Precision: "s",
+	})
+	checkError(err)
+	return bp
+}
+
+func addPoint(batchPoints client.BatchPoints, rawTags map[string]interface{}, count int) {
+	tags := map[string]string{}
+	for key, value := range rawTags {
+		tags[key] = value.(string)
+	}
+	fields := map[string]interface{}{"count": count}
+	pt, err := client.NewPoint("issue_count", tags, fields, time.Now())
+	checkError(err)
+	fmt.Printf("%v: %d issues\n", tags, count)
+	batchPoints.AddPoint(pt)
+}
+
 func main() {
+	// Read json config
 	rawConfig, configErr := ioutil.ReadFile("./config.json")
 	checkError(configErr)
 	var jsonConfig interface{}
@@ -45,12 +86,20 @@ func main() {
 	checkError(configErr)
 	config := jsonConfig.(map[string]interface{})
 	queries := config["queries"].([]interface{})
+
+	// create influx client and batch points (only one send operation at the end)
+	influxClient := createInfluxClient(config)
+	batchPoints := createBatchPoints(config, influxClient)
 	for _, queryObject := range queries {
 		q := queryObject.(map[string]interface{})
+		// run jira query
 		jql := q["jql"].(string)
-		measurement := q["name"]
 		count := runJqlQuery(config, jql)
-		fmt.Printf("%v: %d issues found\n", measurement, count)
+		// create influx point and save for later
+		addPoint(batchPoints, q["tags"].(map[string]interface{}), count)
 	}
+	fmt.Println("Writing data to InfluxDB")
+	// write the points
+	influxClient.Write(batchPoints)
 
 }
