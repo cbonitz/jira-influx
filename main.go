33066178fdcb2cb702bff0ef760ee3cc2054ea14
eschultze <eduardo.schultze@gmail.com>
2016-07-27 18:30:37 -0300

    Influxdb is now influxdata on github.com

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/influxdb/influxdb/client/v2"
+	"github.com/influxdata/influxdb/tree/master/client/v2"
 	"io/ioutil"
 	"net/http"
 	"net/url"
